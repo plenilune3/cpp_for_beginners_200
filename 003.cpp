e241a57975927507f0228bd4e8c9d7c8ce5a5e59
Mingyu Kang <plenilune095@gmail.com>
2019-07-04 20:48:15 +0900

    변수 배우기

---
+++ 003.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int one = 1;
+    int two = 2;
+    int sum = one + two;
+
+    cout << "1 + 2 = " << sum << endl;
+
+    return 0;
+}
