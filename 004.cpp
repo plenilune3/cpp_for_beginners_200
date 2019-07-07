da7e6791dff7be19679f949754ddd7910716dc7e
Mingyu Kang <plenilune095@gmail.com>
2019-07-07 23:03:47 +0900

    상수 배우기(const)

---
+++ 004.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int GREATE_VICTORY_SALSU = 612;
+    const int GREATE_VICTORY_GWIJU = 1019;
+
+    cout << "고구려 살수대첩 연도 : " << GREATE_VICTORY_SALSU << "년" << endl;
+    cout << "고려 귀주대첩 연도 : " << GREATE_VICTORY_GWIJU << "년" << endl;
+
+    return 0;
+}
