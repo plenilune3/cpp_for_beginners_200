111b92e78a6bf0d37ad51b06b505f5b54cbba035
Mingyu Kang <plenilune095@gmail.com>
2019-07-10 19:35:32 +0900

    자료형 이해하기

---
+++ 007.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    char character = 'C';
+    int integer = 100;
+    double precision = 3.14159;
+    bool is_true = true;
+    string word  = "Hello world";
+
+    cout << "char : " << character << endl;
+    cout << "int : " << integer << endl;
+    cout << "double : " << precision << endl;
+    cout << "bool : " << is_true << endl;
+    cout << "string : " << word << endl;
+
+    return 0;
+}
