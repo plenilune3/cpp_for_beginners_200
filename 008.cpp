de3cd327fcf36f094841038d9e5e561b9f051159
Mingyu Kang <plenilune095@gmail.com>
2019-07-11 20:05:21 +0900

    조건문 배우기

---
+++ 008.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int x = 10;
+    int y = 1;
+
+    if (x > y)
+        cout << "x는 y보다 큽니다." << endl;
+    else
+        cout << "x는 y보다 작습니다." << endl;
+
+    return 0;
+}
