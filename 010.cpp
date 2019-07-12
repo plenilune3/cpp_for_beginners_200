f6001cefeb55c229cb7b679f2864d8fb46424b61
Mingyu Kang <plenilune095@gmail.com>
2019-07-12 23:47:40 +0900

    배열 배우기([크기])

---
+++ 010.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int kArraySize = 3;
+
+    int founding[kArraySize];
+    founding[0] = 918;
+    founding[1] = 1392;
+    founding[2] - 1948;
+
+    cout << "고려 건국 연도 : " << founding[0] << endl;
+    cout << "조선 건국 연도 : " << founding[1] << endl;
+    cout << "한국 건국 연도 : " << founding[2] << endl;
+
+    return 0;
+}
