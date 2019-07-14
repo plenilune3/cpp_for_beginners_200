6eb470410db7db4261e0d81ea848abcec7dae91d
Mingyu Kang <plenilune095@gmail.com>
2019-07-14 15:20:00 +0900

    주석배우기(//, /**/)

---
+++ 012.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // 출력하지 않을 코드
+    // cout << "여몽전쟁 영웅 김윤후, 처인성 전투 1232년" << endl;
+
+    // 0부터 9까지 출력
+    /*for (int i = 0; i < 10; i++)
+          cout << i << ", "; */
+
+    return 0;
+}
