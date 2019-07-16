abd7577017a3360a2b5aabb59880ad388f0edba9
Mingyu Kang <plenilune095@gmail.com>
2019-07-17 00:06:40 +0900

    #include 배우기

--- 014.cpp
+++ 014.cpp
@@ -1,11 +1,20 @@
-#include<iostrea>
+#include<iostream>
 #include<vector>
 
 using namespace std;
 
 int main()
 {
+    vector<int> exam;
+    exam.push_back(10);
+    exam.push_back(20);
+    exam.push_back(30);
 
-    retrun 0;
+    for (int i = 0, size = exam.size(); i < size; i++)
+    {
+        cout << "벡터 값 : " << exam.at(i) << endl;
+    }
+
+    return 0;
 }
 
