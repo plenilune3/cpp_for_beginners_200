f1fcaa80fd80efc3134924951143667e2777fe69
Mingyu Kang <plenilune095@gmail.com>
2019-07-19 01:20:19 +0900

    문자형 변수 이해하기(char)

---
+++ 016.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char ch1 = 'c';
+    char ch2 = 200;
+
+    unsigned char ch3 = 'c';
+    unsigned char ch4 = 200;
+
+    printf("char ch1 = %c, %d\n", ch1, ch1);
+    printf("char ch2 = %c, %d\n", ch2, ch2);
+    printf("char ch3 = %c, %d\n", ch3, ch3);
+    printf("char ch4 = %c, %d\n", ch4, ch4);
+
+    return 0;
+}
