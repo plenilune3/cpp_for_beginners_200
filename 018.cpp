0e1f301a83894ceee43e4935b7c84b8241464bd4
Mingyu Kang <plenilune095@gmail.com>
2019-07-22 23:21:51 +0900

    정수형 변수 이해하기(int)

---
+++ 018.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int positive = 100;
+    int negative = -200;
+    int ascii_value = 'A';
+
+    cout << "양수 값 : " << positive << endl;
+    cout << "음수 값 : " << negative << endl;
+    cout << "아스키 값 : " << ascii_value << endl;
+
+    return 0;
+}
+
