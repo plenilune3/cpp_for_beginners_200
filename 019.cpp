220f4d55fe31e7ff20d55c47abac2618988b3af4
Mingyu Kang <plenilune095@gmail.com>
2019-07-23 19:26:28 +0900

    실수형 변수 이해하기(double)

---
+++ 019.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double pi_d = 3.14;
+    float pi_f = 3.14f;
+
+    cout << "pi_d = " << pi_d << endl;
+    cout << "pi_f = " << pi_f << endl;
+
+    return 0;
+}
