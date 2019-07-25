4c1ad5ed4c12ee28dac9cc980d9d0914caee2c9f
Mingyu Kang <plenilune095@gmail.com>
2019-07-25 20:09:06 +0900

    대입 연산자 이해하기(=)

---
+++ 021.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 1;
+    int y = 9;
+
+    int z = x + y;
+
+    double i = 1.2;
+    double j = 2.3;
+
+    double k = i + j;
+
+    cout << "x + y = " << z << endl;
+    cout << "i + j = " << k << endl;
+
+    return 0;
+}
