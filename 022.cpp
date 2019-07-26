c56cfb0c4fc58d213c1af3a2f86e0c7ab0903171
Mingyu Kang <plenilune095@gmail.com>
2019-07-26 19:50:58 +0900

    부호 연산자 이해하기(+, -)

---
+++ 022.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 1;
+    int y = -1;
+    int z = -2;
+
+    cout << "1 + (-1) = " << x + y << endl;
+    cout << "-1 + -2 = " << y + z << endl;
+
+    return 0;
+}
