dc6f6f58f0e5d91cf331aa5667a4cf57a7d65e99
Mingyu Kang <plenilune095@gmail.com>
2019-07-31 23:53:25 +0900

    쉼표 연산자 이해하기(,)

---
+++ 027.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int goguryeo = 37, baekjae = 18, silla = 57;
+
+    printf("삼국 건국연도\n");
+    printf("고구려 bc%d년, 백제 bc%d년, 신라 bc%d년", goguryeo, baekjae, silla);
+
+    return 0;
+}
+
