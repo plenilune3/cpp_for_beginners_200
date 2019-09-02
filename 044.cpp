b083b6bca886551e644b7eb43b855435f8e7a1f1
Mingyu Kang <plenilune095@gmail.com>
2019-09-02 19:49:46 +0900

    실수 반올림하기(round)

---
+++ 044.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout << "round(1.1) : " << round(1.1) << endl;
+    cout << "round(2.3) : " << round(2.3) << endl;
+    cout << "round(40.5) : " << round(40.5) << endl;
+    cout << "round(55.7) : " << round(55.7) << endl;
+    cout << "round(100.9) : " << round(100.9) << endl;
+
+    return 0;
+}
