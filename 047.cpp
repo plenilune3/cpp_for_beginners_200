57066684bce3c529351bb0887eba4c28718c1908
Mingyu Kang <plenilune095@gmail.com>
2019-09-06 15:32:05 +0900

    제곱근 구하기(sqrt)

---
+++ 047.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout << "4의 제곱근 : " << sqrt(4.0) << endl;
+    cout << "16의 제곱근 : " << sqrt(16) << endl;
+    cout << "64의 제곱근 : " << sqrt(64) << endl;
+    cout << "256의 제곱근 : " << sqrt(256) << endl;
+
+    return 0;
+}
