2c4e6a17313e4932092b893cba5270ad896db2ee
Mingyu Kang <plenilune095@gmail.com>
2019-09-18 15:03:57 +0900

    문자열 길이 구하기(length)

---
+++ 055.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string jinduk_name = "김승만";
+    string jinsung_name = "Kim man";
+
+    cout << "진덕여왕 이름 길이 : " << jinduk_name.length() << endl;
+    cout << "진성여왕 이름 길이 : " << jinsung_name.length() << endl;
+
+    return 0;
+}
