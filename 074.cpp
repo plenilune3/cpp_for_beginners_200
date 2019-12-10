59c1661042d4380f7f88375611868784229d932e
Mingyu Kang <plenilune095@gmail.com>
2019-12-10 23:51:43 +0900

    Call by Value 이해하기

---
+++ 074.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+Using namespace std;
+
+void Func(int arg)
+{
+	cout << "변경 전 : " << arg << endl;
+	arg += 10;
+	cout << "변경 후 : " << arg << endl;
+}
+
+int main()
+{
+	int year = 10;
+
+	Func(year);
+
+	cout << "함수 종료 후 : " << year << endl;
+
+	return 0;
+}
