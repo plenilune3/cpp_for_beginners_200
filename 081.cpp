24ee51df9250c5f129f70c7d1347b67e7211901a
Mingyu Kang <plenilune095@gmail.com>
2019-12-16 23:59:35 +0900

    1차원, 2차원 배열 초기화 하기

---
+++ 081.cpp
@@ -0,0 +1,32 @@
+#include <iostrea>
+
+using namespace std;
+
+int main()
+{
+	int data1[3] = { 0, 1, 2, }
+	int data2[2][2] = { { 0, } };
+	int data3[2][2];
+
+	cout << "== data1 ==" << endl;
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			cout << "data2[" << i << "][" << j << "] = "
+				<< data2[i][j] << endl;
+		}
+	}
+
+	cout << endl << "== data3 ==" << endl;
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			cout << "data3[" << i << "][" << j << "] = "
+				<< data2[i][j] << endl;
+		}
+	}
+
+	return 0;
+}
